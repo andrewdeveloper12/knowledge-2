fa73b8c2e55ca63fc5279a62c9fffda8dd583876
andrewdeveloper12 <andrewdematry@gmail.com>
2025-07-12 16:25:42 +0300

    Initial commit - connect to GitHub

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/andrew/', // ← نفس اسم الريبو
+  base: '/knowledge-2/', // ← نفس اسم الريبو
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
